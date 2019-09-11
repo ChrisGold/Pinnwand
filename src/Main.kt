7569a2d1b1eeccc56c7fc7fbb480aeba7652df71
Christian Goldapp <c.goldapp@tu-bs.de>
2019-09-11 23:48:39 +0200

    Create connection

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,8 @@
-fun main() {
-    println("Hello, World!")
+import discord4j.core.DiscordClientBuilder
+import java.io.File
+
+fun main(args: Array<String>) {
+    val token = args.getOrElse(0) { "discord.token.txt" }.let { File(it).readText(Charsets.UTF_8) }
+    val client = DiscordClientBuilder(token).build()
+    client.login().block()
 }
